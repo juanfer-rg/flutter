3e0c9470c3a07410bdf76e4fecb8762043062bb6
Juan Fernando Rojas Garzón <jfrojasgarzon@gmail.com>
2022-04-26 15:03:00 -0500

    199 Shared Preferences
    Falta finalizar el video error en flutter pub get

--- preferences_app/lib/screens/home_screen.dart
+++ preferences_app/lib/screens/home_screen.dart
@@ -7,13 +7,20 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      drawer: const SideMenu(),
-      body: Center(
-        child: Text('Home Screen'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        drawer: const SideMenu(),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('isDarMode: '),
+            Divider(),
+            Text('Genero: '),
+            Divider(),
+            Text('Nombre usuario: '),
+            Divider(),
+          ],
+        ));
   }
 }
--- preferences_app/lib/screens/settings_screen.dart
+++ preferences_app/lib/screens/settings_screen.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:preferences_app/widgets/widgets.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   static const String routeName = 'Settings';
 
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool isDarkmode = false;
+  int gender = 1;
+  String name = 'Pepe';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,8 +19,63 @@ class SettingsScreen extends StatelessWidget {
         title: Text('Settings'),
       ),
       drawer: const SideMenu(),
-      body: Center(
-        child: Text('Settings Screen'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Ajustes',
+                style: TextStyle(fontSize: 45, fontWeight: FontWeight.w300),
+              ),
+              const Divider(),
+              SwitchListTile.adaptive(
+                title: const Text('Darkmode'),
+                value: isDarkmode,
+                onChanged: (value) {
+                  isDarkmode = value;
+                  setState(() {}); //Redibuja el widget
+                },
+              ),
+              Divider(),
+              RadioListTile<int>(
+                value: 1,
+                groupValue: gender,
+                title: const Text('Masculino'),
+                onChanged: (value) {
+                  gender = value ?? 1;
+                  setState(() {});
+                },
+              ),
+              Divider(),
+              RadioListTile<int>(
+                value: 2,
+                groupValue: gender,
+                title: const Text('Femenino'),
+                onChanged: (value) {
+                  gender = value ?? 2;
+                  setState(() {});
+                },
+              ),
+              Divider(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: TextFormField(
+                  initialValue: 'Fer',
+                  onChanged: (value) {
+                    name = value;
+                    setState(() {});
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Nombre',
+                    helperText: 'Nombre usuario',
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
