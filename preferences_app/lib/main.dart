20a193a95d34ebcac806f350aba94a1e2ab1bc76
Juan Fernando Rojas <jfrojasgarzon@gmail.com>
2022-04-26 20:58:19 -0500

    199 FInalizado y funional

--- preferences_app/lib/main.dart
+++ preferences_app/lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:preferences_app/screens/screens.dart';
+import 'package:preferences_app/share_preferences/preferences.dart';
 
-void main() => runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Preferences.init();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
@@ -16,6 +22,7 @@ class MyApp extends StatelessWidget {
         HomeScreen.routeName: (_) => HomeScreen(),
         SettingsScreen.routeName: (_) => SettingsScreen(),
       },
+      theme: ThemeData.light(),
     );
   }
 }
