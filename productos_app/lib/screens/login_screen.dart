f3e56589f41f7db143c40337e865fef7e2899950
Juan Fernando Rojas Garzón <jfrojasgarzon@gmail.com>
2022-04-27 15:15:23 -0500

    205 Fondo Login

---
+++ productos_app/lib/screens/login_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:productos_app/widgets/widgets.dart';
+
+class LoginScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AuthBackground()
+   );
+  }
+}
